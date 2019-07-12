288cb04e08b8f6b9dac94635b5e58636ee2e3d3b
macenrola <hugues@1dcore.com>
2019-07-12 14:15:29 +0800

    working MMFF94 frequencies

--- hessian.py
+++ hessian.py
@@ -6,10 +6,6 @@ Created on Wed Jul 10 16:57:27 2019
 @author: macenrola
 """
 
-import rdkit
-from rdkit import Chem
-from rdkit.Chem import AllChem
-import numpy as np 
 
 def get_weight_dic():
 	"""
@@ -50,7 +46,9 @@ def replace_coords_in_mol_block(RDKIT_BLOCK_IN, coord_matrix):
 				)
 
 	RDKIT_BLOCK_OUT = ''.join(RDKIT_BLOCK)
-
+# =============================================================================
+# 	print RDKIT_BLOCK_OUT
+# =============================================================================
 	return RDKIT_BLOCK_OUT	
 
 def get_energy(rdkit_mol_block):
@@ -64,6 +62,8 @@ def get_energy(rdkit_mol_block):
 	ff.Initialize()
 	return ff.CalcEnergy()
 
+
+
 def make_mass_matrix_from_atom_list(atom_list):
 	"""
 	PRE: Takes in an atom list
@@ -73,10 +73,7 @@ def make_mass_matrix_from_atom_list(atom_list):
 	dic_weights={'C':12,'H':1,'N':14, 'O':16}
 	diag=[]
 	for ats in atom_list:
-# =============================================================================
-# 		diag.extend([1.0/(dic_weights[ats])]*3) # those masses are atomic masses so the factor 1.66e-27
-# =============================================================================
-		diag.extend([1.0/dic_weights[ats]/1.66e-27]*3) # those masses are atomic masses so the factor 1.66e-27
+		diag.extend([1.0/dic_weights[ats]]*3) # those masses are atomic masses so the factor 1.66e-27
 
 	mass_matrix = np.diag(diag)
 
@@ -90,76 +87,44 @@ def compute_hessian(mol):
 	POST: returns its hessian, mass weighted. The energies are given as kcal/mol and displacements in Angstrom. 
 	I think this is good for numerical stability, the results should be scaled appropriately after
 	"""
-	dx=0.0001
-	dy=dx
-	print Chem.MolToMolBlock(mol)
+	
 	atom_list, atom_coords = get_xyz_from_mol(mol)
-	print atom_list
+	print atom_list, Chem.MolToSmiles(mol)
 	ORIG_MOL_BLOCK = Chem.MolToMolBlock(mol)
 
-	print replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.array([[x+1 for x in y] for y in atom_coords]))
-
 	lin_coords = np.reshape(atom_coords, (1, len(atom_coords)*3))[0]
-	hessian = np.zeros((len(lin_coords), len(lin_coords)))
-# =============================================================================
-# 	The elements are defined as :
-# 			Ex1x2   = E(x+dx, y+dy) 
-# 			Ex_1x2  = E(x-dx, y+dy)
-# 			Ex1x_2  = E(x+dx, y-dy)
-# 			Ex_1x_2 = E(x-dx, y-dy)
-# 	and the hessian ddE/dxdy = ([E(x+dx, y+dy) - E(x+dx, y-dy)]/dy - [E(x-dx, y+dy) - E(x-dx, y-dy)]/dy )/dx 
-# =============================================================================
-	for i in range(len(lin_coords)):
-		for j in range(len(lin_coords)):
-			Ex1x2_coords, Ex_1x2_coords, Ex1x_2_coords, Ex_1x_2_coords = [lin_coords.copy(), lin_coords.copy(), lin_coords.copy(), lin_coords.copy()]
-			
-			Ex1x2_coords[i]+=dx
-			Ex1x2_coords[j]+=dy
-			Ex_1x2_coords[i]-=dx
-			Ex_1x2_coords[j]+=dy
-			Ex1x_2_coords[i]+=dx
-			Ex1x_2_coords[j]-=dy
-			Ex_1x_2_coords[i]-=dx
-			Ex_1x_2_coords[j]-=dy
-
-			shape = (len(lin_coords)/3,3)
-# =============================================================================
-# 			print get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex1x2_coords, shape))) 
-# 			print get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex1x_2_coords, shape)))
-# 			print get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex_1x2_coords, shape))) 
-# 			print get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex_1x_2_coords, shape)))
-# =============================================================================
-			Ex1x2   = get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex1x2_coords, shape)))
-			Ex_1x2  = get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex_1x2_coords, shape)))
-			Ex1x_2  = get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex1x_2_coords, shape)))
-			Ex_1x_2 = get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex_1x_2_coords, shape)))
-			ddE_dxdy = ((Ex1x2-Ex_1x2)/dy-(Ex1x_2-Ex_1x_2)/dy)/dx
-# =============================================================================
-# 			ddE_dxdy = ( 	
-# 				(get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex1x2_coords, shape))) - get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex1x_2_coords, shape))))/dx  -
-# 					(get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex_1x2_coords, shape))) - get_energy(replace_coords_in_mol_block(ORIG_MOL_BLOCK, np.reshape(Ex_1x_2_coords, shape))))/dx 
-# 			   )/dx
-# =============================================================================
-			hessian[i,j] = ddE_dxdy
-			
-	massmat = np.array(make_mass_matrix_from_atom_list(atom_list)[1])
-	print massmat
-	avg_hessian = (hessian + hessian.T)/2 *4185/1e-20/6.022e23
-	print avg_hessian
-	mass_weighted = np.matmul(np.matmul(massmat, avg_hessian), massmat)
+	shape = (len(lin_coords)/3,3)
+	def get_energy_from_coords(coords, shape, orig_rdkit_block):
+		"""
+		PRE: takes in coordinates as 1d array, shape, orinigal mol block, coordinates atom number has the same order as the rdkit mol block
+		POST: Returns the energy
+		"""
+		return get_energy(replace_coords_in_mol_block(orig_rdkit_block, np.reshape(coords, shape))) 
+	
+	hessian = nd.Hessian(get_energy_from_coords)(lin_coords, shape, ORIG_MOL_BLOCK)
+	massmat, massmat_sqr = make_mass_matrix_from_atom_list(atom_list)
+	avg_hessian = (hessian + hessian.T)/2 *4185*1e8/1e-10/6.022e23          # Units conversion factors taken from http://openmopac.net/manual/Hessian_Matrix.html
+	mass_weighted = np.matmul(np.matmul(massmat_sqr, avg_hessian), massmat_sqr)
 	return mass_weighted
 
 if __name__ == "__main__":
-	test_mol = Chem.MolFromSmiles('C')
+	import rdkit
+	from rdkit import Chem
+	from rdkit.Chem import AllChem
+	import numpy as np
+	import numdifftools as nd 
+	
+	test_mol = Chem.MolFromSmiles('N')
 	test_mol = Chem.AddHs(test_mol)
 	AllChem.EmbedMolecule(test_mol)
+	print "BEFORE OPTI"
+	print Chem.MolToMolBlock(test_mol)
 	n_steps=1000000
 	tol=1e-10
 	AllChem.MMFFSanitizeMolecule(test_mol)
 	ff = AllChem.MMFFGetMoleculeForceField(test_mol, pyMMFFMolProperties=AllChem.MMFFGetMoleculeProperties(test_mol, mmffVariant='MMFF94', mmffVerbosity = 0), ignoreInterfragInteractions=False, nonBondedThresh=100.0)
 	ff.Initialize()
 	cf = ff.Minimize(n_steps, tol, tol)	
-# =============================================================================
-# 	print sorted([float('{0:4.4f}'.format(np.sqrt(x/5.892e-5))) for x in np.linalg.eig(np.array(compute_hessian(test_mol)))[0]])
-# =============================================================================
-	print sorted([float('{0:4.4f}'.format(np.sqrt(x)/2/np.pi/3e10)) for x in np.linalg.eig(np.array(compute_hessian(test_mol)))[0]])
+	print "AFTER OPTI ({})".format(cf)
+	print Chem.MolToMolBlock(test_mol)
+	print [float('{0:4.4f}'.format(np.sqrt(x*6.022e28)/2/np.pi/2.99e10)) for x in sorted(np.linalg.eig(np.array(compute_hessian(test_mol)))[0])] # Conversion factors taken from http://openmopac.net/manual/Hessian_Matrix.html
